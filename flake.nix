0056d98967368e298a3fc2b033d992df65d3a5a8
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2024-11-16 22:00:54 -0800

    It runs

--- flake.nix
+++ flake.nix
@@ -10,10 +10,38 @@
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
+        libPath = with pkgs; 
+          [ 
+            libffi
+            wayland-protocols
+            wayland
+            libGL
+            xorg.libxcb
+            glfw
+
+            pkg-config
+
+            xorg.libX11
+            xorg.libX11.dev
+            xorg.libXft
+            xorg.libXrandr
+            xorg.libXinerama
+            xorg.libXcursor
+            xorg.libXi
+
+            libxkbcommon
+
+            libglvnd
+          ]; 
       in
-      {
+        {
         defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs; mkShell {
+          nativeBuildInputs = [
+            wayland-protocols
+            libxkbcommon
+            wayland
+          ];
           buildInputs = [ 
             cargo 
             rustc 
@@ -21,8 +49,10 @@
             pre-commit 
             rustPackages.clippy 
             cmake
-          ];
+          ]++ libPath;
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          LD_LIBRARY_PATH = lib.makeLibraryPath libPath;
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
         };
       }
     );
